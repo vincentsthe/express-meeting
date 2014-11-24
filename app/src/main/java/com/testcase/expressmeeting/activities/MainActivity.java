1bacf343ca15167aadc4eaaa74d06e88a63e165c
Jordan Fernando <fernandojordan.92@gmail.com>
2014-11-24 23:56:39 +0700

    Fix Maps, add simple location

--- app/src/main/java/com/testcase/expressmeeting/activities/MainActivity.java
+++ app/src/main/java/com/testcase/expressmeeting/activities/MainActivity.java
@@ -1,49 +1,112 @@
 package com.testcase.expressmeeting.activities;
 
-import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
+import android.os.Looper;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.ListView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.testcase.expressmeeting.R;
-import com.testcase.expressmeeting.fragments.GMapFragment;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements LocationListener {
+
+    private static final String MAP_TAG = "GMap";
+
+    private Location currentLoc;
+
+    private Toolbar mToolbar;
 
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        if (toolbar != null) {
-            setSupportActionBar(toolbar);
+        setupActionBar();
+        setupDrawer();
+        if (savedInstanceState == null) {
+            setupMap();
         }
 
+        setupLocation();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        configMap();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLoc = location;
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    private void setupActionBar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+        }
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    private void setupDrawer() {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.drawable.ic_launcher, R.drawable.ic_launcher) {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.drawable.ic_launcher, R.drawable.ic_launcher) {
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
@@ -55,35 +118,34 @@ public class MainActivity extends ActionBarActivity {
             }
         };
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                .add(R.id.container, new GMapFragment())
-                .commit();
-        }
-
         mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
+    private void setupLocation() {
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        currentLoc = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if (currentLoc == null) {
+            currentLoc = new Location("coba");
+            currentLoc.setLatitude(0f);
+            currentLoc.setLongitude(0f);
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupMap() {
+        GoogleMapOptions googleMapOptions = new GoogleMapOptions();
+        googleMapOptions.zoomControlsEnabled(false);
+
+        MapFragment mapFragment = MapFragment.newInstance(googleMapOptions);
+
+        getFragmentManager().beginTransaction()
+            .add(R.id.container, mapFragment, MAP_TAG)
+            .commit();
+    }
+
+    private void configMap() {
+        mMap = ((MapFragment)getFragmentManager().findFragmentByTag(MAP_TAG)).getMap();
+        mMap.setMyLocationEnabled(true);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLoc.getLatitude(), currentLoc.getLongitude()), 10.0f));
     }
 }
